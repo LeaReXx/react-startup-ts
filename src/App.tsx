949d68c6906a935ea472f4a8637c66f146f1b26d
Sepehr Aghapour <petrol_cs@yahoo.com>
2023-09-07 19:50:57 +0330

    change file location

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import AboutServices from "./components/abouse services/AboutServices";
 import Header from "./components/header/Header";
 import Customers from "./components/customers/Customers";
 import AboutInvesting from "./components/about investing/AboutInvesting";
-import MakeMoney from "./make money/MakeMoney";
+import MakeMoney from "./components/make money/MakeMoney";
 
 function App() {
   return (
